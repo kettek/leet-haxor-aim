3478d0c1a543ec74bf9a4a1de2f31548befa5d25
kts of kettek <kettek1@kettek.net>
2018-02-08 22:34:19 -0800

    Change code to use gModes[...] object.
    Game mode logic is now contained in the gModes associative array. This is
    simply an object that contains any of the following methods:
      * start
      * reset
      * draw
      * loop(delta)
      * tap(x, y)
    
    The current mode is determined by the last focused mode element by taking
    its id of "mode-...", stripping off the initial "mode-" string, and using
    that to reference the associative array.
    
    Additionally, the global "lVariableName" syntax has been replaced with
    "gVariableName", to better represent the intent. "lVariableName" is now
    used as local variables within modes.

--- index.js
+++ index.js
@@ -20,39 +20,24 @@ var eSettingsSpawnrate  = document.getElementById('settings-spawnrate');
 var eSettingsLifetime   = document.getElementById('settings-lifetime');
 var eSettingsLength     = document.getElementById('settings-length');
 
-var lAnimationFrame;
-var lCtx            = eShoot.getContext('2d');
-var lPx             = 1.0;
-var lPy             = 1.0;
-var lPs             = 1.0;
+var gAnimationFrame;
+var gCtx            = eShoot.getContext('2d');
+var gPx             = 1.0;
+var gPy             = 1.0;
+var gPs             = 1.0;
 
-var lSelectedMode   = '';
-var lSettingsCache  = {};
+var gSettingsCache  = {};
 
-var lRunning        = false;
-var lCircles        = [];
+var gRunning        = false;
 
-var lHitCount, lLostCount, lMissCount;
-var lStartTime, lLastTime, lCurrentTime;
-var lLastCreate;
+var gHitCount, gLostCount, gMissCount;
+var gStartTime, gLastTime, gCurrentTime;
 
-var lHitTimeLast = 0;
-var lHitTimes = [];
+var gHitTimeLast = 0;
+var gHitTimes = [];
 
-var lModes = {
-  'mode-track': {
-    start: function() { },
-    reset: function() { },
-    loop: function(d) { },
-    draw: function(d) { },
-  },
-  'mode-target': {
-    start: function() { },
-    reset: function() { },
-    loop: function(d) { },
-    draw: function(d) { },
-  }
-};
+var gModes          = {};
+var gSelectedMode   = '';
 
 // Do some initial setup for page elements.
 {
@@ -65,7 +50,7 @@ var lModes = {
       eSettingsModes.push(c[i]);
       if (i == 0) {
         showFieldset(c[i]);
-        lSelectedMode = c[i].id;
+        gSelectedMode = c[i].id.substring(5);
       } else {
         hideFieldset(c[i]);
       }
@@ -74,6 +59,7 @@ var lModes = {
           if (eSettingsModes[i] !== self) {
             hideFieldset(eSettingsModes[i]);
           } else {
+            gSelectedMode = self.id.substring(5);
             showFieldset(self);
           }
         }
@@ -90,17 +76,17 @@ function hideHud() {
   eHud.style.display = 'none';
 }
 function showScoreboard() {
-  eScoreHits.innerText = lHitCount;
-  eScoreHitsP.innerText = Math.round(lHitCount / (lHitCount+lLostCount) * 100) + '%';
-  eScoreMisses.innerText = lMissCount;
-  eScoreLosses.innerText = lLostCount;
-  eScoreLossesP.innerText = Math.round(lLostCount / (lLostCount+lHitCount) * 100) + '%';
+  eScoreHits.innerText = gHitCount;
+  eScoreHitsP.innerText = Math.round(gHitCount / (gHitCount+gLostCount) * 100) + '%';
+  eScoreMisses.innerText = gMissCount;
+  eScoreLosses.innerText = gLostCount;
+  eScoreLossesP.innerText = Math.round(gLostCount / (gLostCount+gHitCount) * 100) + '%';
   {
     var total = 0;
-    for (var i = 0; i < lHitTimes.length; i++) {
-      total += lHitTimes[i];
+    for (var i = 0; i < gHitTimes.length; i++) {
+      total += gHitTimes[i];
     }
-    total = lHitTimes.length > 0 ? Math.round(total / lHitTimes.length) : 0;
+    total = gHitTimes.length > 0 ? Math.round(total / gHitTimes.length) : 0;
     eScoreHittime.innerText = total + 'ms';
   }
   eScoreboard.style.display = '';
@@ -115,7 +101,7 @@ function hideSettings() {
   eSettings.style.display = 'none';
 }
 function getSetting(name) {
-  if (lSettingsCache[name] !== undefined) return lSettingsCache[name];
+  if (gSettingsCache[name] !== undefined) return gSettingsCache[name];
   var els = eSettings.getElementsByTagName('input');
   var vals = [];
   for (var i = 0; i < els.length; i++) {
@@ -131,8 +117,8 @@ function getSetting(name) {
   for (var i = 0; i < vals.length; i++) {
     total += vals[i];
   }
-  lSettingsCache[name] = total / vals.length;
-  return lSettingsCache[name];
+  gSettingsCache[name] = total / vals.length;
+  return gSettingsCache[name];
 }
 function setSetting(name, value) {
   var els = eSettings.getElementsByTagName('input');
@@ -142,7 +128,7 @@ function setSetting(name, value) {
       els[i].dispatchEvent(new Event('change'));
     }
   }
-  delete lSettingsCache[name];
+  delete gSettingsCache[name];
 }
 window.setSetting = setSetting;
 function hideFieldset(target) {
@@ -165,90 +151,52 @@ function showFieldset(target) {
 function adjust() {
   eShoot.width = eShoot.parentNode.clientWidth;
   eShoot.height = eShoot.parentNode.clientHeight;
-  lPx = eShoot.width / 100;
-  lPy = eShoot.height / 100;
-  lPs = lPx > lPy ? lPx : lPy;
+  gPx = eShoot.width / 100;
+  gPy = eShoot.height / 100;
+  gPs = gPx > gPy ? gPx : gPy;
 }
 
 function sync() {
-  eHudHits.innerText = lHitCount;
-  eHudMisses.innerText = lMissCount;
-  eHudLost.innerText = lLostCount;
+  eHudHits.innerText = gHitCount;
+  eHudMisses.innerText = gMissCount;
+  eHudLost.innerText = gLostCount;
 }
 
 function reset() {
-  lStartTime = lLastTime = lCurrentTime = lLastCreate = performance.now();
-  lHitCount = lLostCount = lMissCount = 0;
-  lHitTimes = [];
-  lCircles = [];
+  gStartTime = gLastTime = gCurrentTime = performance.now();
+  gHitCount = gLostCount = gMissCount = 0;
+  gHitTimes = [];
 }
 
 function start() {
   reset();
-  lRunning = true;
-  lAnimationFrame = window.requestAnimationFrame(loop);
+  gRunning = true;
+  gAnimationFrame = window.requestAnimationFrame(loop);
 }
 
 function draw() {
   // Clear our canvas.
-  lCtx.clearRect(0, 0, eShoot.width, eShoot.height);
-  // Render our circles.
-  for (var i = 0; i < lCircles.length; i++) {
-    lCircles[i].draw();
-  }
+  gCtx.clearRect(0, 0, eShoot.width, eShoot.height);
+  if (gModes[gSelectedMode].draw) gModes[gSelectedMode].draw();
 }
 
 function loop(pTime) {
   adjust();
-  lCurrentTime = pTime;
-  var elapsedTime = lCurrentTime - lLastTime;
+  gCurrentTime = pTime;
 
-  // Create a new circle if enough time has elapsed.
-  if (lCurrentTime >= lLastCreate + getSetting('spawnrate')) {
-    createCircle();
-  }
-
-  // Reverse iteration through our circles, running their logic and removing them as needed.
-  for (var i = lCircles.length-1; i >= 0; i--) {
-    if (!lCircles[i].step(elapsedTime)) {
-      lLostCount++;
-    }
-    if (lCircles[i].remove) {
-      lCircles.splice(i, 1);
-    }
-  }
+  if (gModes[gSelectedMode].loop) gModes[gSelectedMode].loop(gCurrentTime - gLastTime);
 
   draw();
   sync();
 
   // Keep on looping.
-  lLastTime = lCurrentTime;
-  if (lRunning && lCurrentTime <= lStartTime+(getSetting('length')*1000)) {
-    lAnimationFrame = window.requestAnimationFrame(loop);
+  gLastTime = gCurrentTime;
+  if (gRunning && gCurrentTime <= gStartTime+(getSetting('length')*1000)) {
+    gAnimationFrame = window.requestAnimationFrame(loop);
   } else {
     hideHud();
     showScoreboard();
   }
-}
-
-function createCircle() {
-  var x, y;
-
-  if (window.crypto) {
-    var ran = new Uint32Array(2);
-    window.crypto.getRandomValues(ran);
-    x = getSetting('size')/2 + ran[0] % (100 - getSetting('size'));
-    y = getSetting('size')/2 + ran[1] % (100 - getSetting('size'));
-  } else {
-    var min = Math.ceil(getSetting('size')/2);
-    var maxW = Math.floor(100 - getSetting('size'));
-    var maxH = Math.floor(100 - getSetting('size'));
-    x = Math.floor(Math.random() * (maxW - min + 1)) + min;
-    y = Math.floor(Math.random() * (maxH - min + 1)) + min;
-  }
-
-  lCircles.push(new Circle(x, y, getSetting('size'), getSetting('lifetime')*1000));
-  lLastCreate = lCurrentTime;
 }
 
 /* ==== Browser Events ==== */
@@ -257,18 +205,7 @@ function tapHandler(e) {
   e.preventDefault();
   var x = e.clientX || e.touches[0].clientX;
   var y = e.clientY || e.touches[0].clientY;
-  for (var i = 0; i < lCircles.length; i++) {
-    if (lCircles[i].collision(x, y)) {
-      lCircles[i].remove = true;
-      lHitCount++;
-      var hit_time = performance.now();
-      lHitTimes.push(hit_time - (lHitTimes.length > 0 ? lHitTimeLast : lStartTime));
-      lHitTimeLast = hit_time;
-      if (lCircles.length-1 == 0) createCircle();
-      return;
-    }
-  }
-  lMissCount++;
+  if (gModes[gSelectedMode].tap) gModes[gSelectedMode].tap(x, y);
 }
 eShoot.addEventListener('touchstart', tapHandler);
 eShoot.addEventListener('mousedown', tapHandler);
@@ -298,19 +235,96 @@ Circle.prototype.step = function(time) {
   return true;
 }
 Circle.prototype.draw = function() {
-  lCtx.beginPath();
-  lCtx.arc(this.x*lPx, this.y*lPy, this.size*lPs, 0, (Math.PI/180)*360);
-  lCtx.closePath();
-  lCtx.fillStyle = "yellow";
-  lCtx.fill();
+  gCtx.beginPath();
+  gCtx.arc(this.x*gPx, this.y*gPy, this.size*gPs, 0, (Math.PI/180)*360);
+  gCtx.closePath();
+  gCtx.fillStyle = "yellow";
+  gCtx.fill();
 }
 Circle.prototype.collision = function(x, y) {
-  var distance = Math.sqrt(Math.pow(x - (this.x*lPx), 2) + Math.pow(y - (this.y*lPy), 2));
-  if (distance < 1 + this.size*lPs) {
+  var distance = Math.sqrt(Math.pow(x - (this.x*gPx), 2) + Math.pow(y - (this.y*gPy), 2));
+  if (distance < 1 + this.size*gPs) {
     return true;
   }
   return false;
 }
+
+/* ==== Modes ==== */
+/* == Target Mode == */
+gModes['target'] = (function(){
+  var lCircles = [];
+  var lLastCreateCounter = 0;
+  function createCircle() {
+    var x, y;
+  
+    if (window.crypto) {
+      var ran = new Uint32Array(2);
+      window.crypto.getRandomValues(ran);
+      x = getSetting('size')/2 + ran[0] % (100 - getSetting('size'));
+      y = getSetting('size')/2 + ran[1] % (100 - getSetting('size'));
+    } else {
+      var min = Math.ceil(getSetting('size')/2);
+      var maxW = Math.floor(100 - getSetting('size'));
+      var maxH = Math.floor(100 - getSetting('size'));
+      x = Math.floor(Math.random() * (maxW - min + 1)) + min;
+      y = Math.floor(Math.random() * (maxH - min + 1)) + min;
+    }
+  
+    lCircles.push(new Circle(x, y, getSetting('size'), getSetting('lifetime')*1000));
+    lLastCreateCounter -= getSetting('spawnrate');
+  }
+
+  return {
+    start: function() {
+    },
+    reset: function() {
+      lCircles = [];
+    },
+    draw: function() {
+      for (var i = 0; i < lCircles.length; i++) {
+        lCircles[i].draw();
+      }
+    },
+    loop: function(delta) {
+      lLastCreateCounter += delta;
+      // Create a new circle if enough time has elapsed.
+      while (lLastCreateCounter >= getSetting('spawnrate')) {
+        createCircle();
+      }
+      // Reverse iteration through our circles, running their logic and removing them as needed.
+      for (var i = lCircles.length-1; i >= 0; i--) {
+        if (!lCircles[i].step(delta)) {
+          gLostCount++;
+        }
+        if (lCircles[i].remove) {
+          lCircles.splice(i, 1);
+        }
+      }
+    },
+    tap: function(x, y) {
+      for (var i = 0; i < lCircles.length; i++) {
+        if (lCircles[i].collision(x, y)) {
+          lCircles[i].remove = true;
+          gHitCount++;
+          var hit_time = performance.now();
+          gHitTimes.push(hit_time - (gHitTimes.length > 0 ? gHitTimeLast : gStartTime));
+          gHitTimeLast = hit_time;
+          if (lCircles.length-1 == 0) createCircle();
+          return;
+        }
+      }
+      gMissCount++;
+    }
+  }
+})();
+/* == Track Mode == */
+gModes['track'] = (function() {
+  return {};
+})();
+/* == Reflex Mode == */
+gModes['reflex'] = (function() {
+  return {};
+})();
 
 adjust();
 reset();
